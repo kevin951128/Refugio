55108695f7c00d3fe0fff1431c518a0b9478460f
Kevin Andres Vargas Triana <kavt_1995@hotmail.com>
2018-04-13 11:55:44 -0500

    Listar
    Se adiciona la funcion de listado en las mascotas

--- Apps/mascota/urls.py
+++ Apps/mascota/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from Apps.mascota.views import index, mascota_view
+from Apps.mascota.views import index, mascota_view, mascota_list
 
 urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^nuevo$', mascota_view, name="mascota_crear"),
+    url(r'^listar$', mascota_list, name="mascota_listar"),
 
 ]
--- Apps/mascota/views.py
+++ Apps/mascota/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from Apps.mascota.form import MascotaForm
+from Apps.mascota.models import Mascota
+
 
 
 # Create your views here.
@@ -19,3 +21,8 @@ def mascota_view(request):
         form = MascotaForm()
 
     return render(request, 'mascota/mascota_form.html', {'form':form})
+
+def mascota_list(request):
+    mascota = Mascota.objects.all()
+    contexto = {'mascotas':mascota}
+    return render(request, 'mascota/mascota_list.html', contexto)
