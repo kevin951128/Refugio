ac168a2090d8c7883da121f5cd22f310b7b51eab
Kevin Andres Vargas Triana <kavt_1995@hotmail.com>
2018-04-17 21:46:36 -0500

    CRUD
    Finalización de operaciones CRUD en aplicación mascota

--- Apps/mascota/views.py
+++ Apps/mascota/views.py
@@ -16,13 +16,34 @@ def mascota_view(request):
         form = MascotaForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('mascota:index')
+        return redirect('mascota:mascota_listar')
     else:
         form = MascotaForm()
 
     return render(request, 'mascota/mascota_form.html', {'form':form})
 
 def mascota_list(request):
-    mascota = Mascota.objects.all()
+    mascota = Mascota.objects.all().order_by('id')
     contexto = {'mascotas':mascota}
     return render(request, 'mascota/mascota_list.html', contexto)
+
+def mascota_edit(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'GET':
+        form = MascotaForm(instance=mascota)
+    else:
+        form = MascotaForm(request.POST, instance=mascota)
+        if form.is_valid():
+            form.save()
+        return redirect('mascota:mascota_listar')
+    return render(request, 'mascota/mascota_form.html', {'form':form})
+
+def mascota_delete(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'POST':
+        mascota.delete()
+        return redirect('mascota:mascota_listar')
+    return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
+
+
+
